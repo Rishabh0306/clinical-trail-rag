1464cfa0ce98ea1aed8381ff2d30141bac0e648d
Rishabh0306 <rishabhgoel3699@gmail.com>
2024-06-14 21:34:08 +0530

    Bugfix

--- src/clinical_trail_rag.py
+++ src/clinical_trail_rag.py
@@ -107,35 +107,35 @@ class ClinicalTrialRAG:
         # Retrieval Evaluation
         qa_dataset = EmbeddingQAFinetuneDataset.from_json(qa_dataset_path)
 
-        retriever = self.index.as_retriever(similarity_top_k=10)
-
-        retriever_evaluator = RetrieverEvaluator.from_metric_names(
-            ["mrr", "hit_rate"], retriever=retriever
-        )
-
-        eval_results = await retriever_evaluator.aevaluate_dataset(qa_dataset)
-
-        def extract_results(name, eval_results):
-            """Extract results from evaluate."""
-
-            metric_dicts = []
-            for eval_result in eval_results:
-                metric_dict = eval_result.metric_vals_dict
-                metric_dicts.append(metric_dict)
-
-            full_df = pd.DataFrame(metric_dicts)
-
-            hit_rate = full_df["hit_rate"].mean()
-            mrr = full_df["mrr"].mean()
-
-            metric_df = pd.DataFrame(
-                {"Retriever Name": [name], "Hit Rate": [hit_rate], "MRR": [mrr]}
-            )
-
-            return metric_df
-
-        retrieval_results_df = extract_results("Retriever Results", eval_results)
-        retrieval_results_df.to_csv(retrieval_results_path, index=False)
+        # retriever = self.index.as_retriever(similarity_top_k=10)
+        #
+        # retriever_evaluator = RetrieverEvaluator.from_metric_names(
+        #     ["mrr", "hit_rate"], retriever=retriever
+        # )
+        #
+        # eval_results = await retriever_evaluator.aevaluate_dataset(qa_dataset)
+        #
+        # def extract_results(name, eval_results):
+        #     """Extract results from evaluate."""
+        #
+        #     metric_dicts = []
+        #     for eval_result in eval_results:
+        #         metric_dict = eval_result.metric_vals_dict
+        #         metric_dicts.append(metric_dict)
+        #
+        #     full_df = pd.DataFrame(metric_dicts)
+        #
+        #     hit_rate = full_df["hit_rate"].mean()
+        #     mrr = full_df["mrr"].mean()
+        #
+        #     metric_df = pd.DataFrame(
+        #         {"Retriever Name": [name], "Hit Rate": [hit_rate], "MRR": [mrr]}
+        #     )
+        #
+        #     return metric_df
+        #
+        # retrieval_results_df = extract_results("Retriever Results", eval_results)
+        # retrieval_results_df.to_csv(retrieval_results_path, index=False)
 
         # Response Evaluation
         queries = list(qa_dataset.queries.values())
